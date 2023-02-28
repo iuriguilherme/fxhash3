77f8da37e711c4ee470d35d096916884fd6c9967
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2023-02-27 21:48:32 -0300

    Fixing jump animation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**!
  * @file Uni the vegan unicorn  
- * @version 1.0.0.0  
+ * @version 1.1.0.0  
  * @copyright Iuri Guilherme 2023  
  * @license GNU AGPLv3  
  * @author Iuri Guilherme <https://iuri.neocities.org/>  
@@ -24,6 +24,8 @@
 //~ import p5 from 'p5';
 import { create as mcreate, all } from 'mathjs';
 const math = mcreate(all, {})
+//~ const phaser = require("phaser")
+import Phaser from 'phaser';
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 // https://github.com/fxhash/fxhash-webpack-boilerplate/issues/20
@@ -33,33 +35,14 @@ const variantFactor = 3.904e-87; // This number is magic
 const fxhashDecimal = base58toDecimal(fxhashTrunc);
 const featureVariant = fxHashToVariant(fxhashDecimal, 1);
 //~ const featureVariant = -1;
-let size, scale, ratio, reWidth, reHeight;
-let width = window.innerWidth;
-let height = window.innerHeight;
 
-//~ let phaser = require("phaser")
-import Phaser from 'phaser';
-
-let player;
-let stars;
-let bombs;
-let platforms;
-let cursors;
-let vegcoins = 0;
-let gameOver = false;
-let vegcoinsText;
-let animaisText;
-let introText;
-let over1Text;
-let over2Text;
-
-let config = {
+const config = {
   "type": Phaser.AUTO,
   "width": 800,
   "height": 600,
-  "parent": 'uni-canvas',
+  "parent": "canvas",
   "physics": {
-    "default": 'arcade',
+    "default": "arcade",
     "arcade": {
       "gravity": { y: 300 },
       "debug": false
@@ -74,117 +57,184 @@ let config = {
 
 let game = new Phaser.Game(config);
 
-function preload ()
-{
-  this.load.image('sky', 'sky.png');
-  this.load.image('ground', 'platform.png');
-  this.load.image('ground2', 'platform2.png');
-  this.load.image('star', 'star.png');
-  this.load.image('bomb', 'bomb.png');
-  this.load.image('camiseta', 'camiseta.png');
+let bombs;
+let cursors;
+let platforms;
+let player;
+let stars;
+
+let animaisText;
+let vegcoinsText;
+let introText;
+let over1Text;
+let over2Text;
+
+let vegcoins = 0;
+let gameOver = false;
+let lastMove = "right";
+
+function preload () {
+  this.load.image("sky", "sky.png");
+  this.load.image("ground", "platform.png");
+  this.load.image("ground2", "platform2.png");
+  this.load.image("star", "star.png");
+  this.load.image("bomb", "bomb.png");
+  this.load.image("camiseta", "camiseta.png");
+  //~ this.load.spritesheet(
+    //~ "dude",
+    //~ "dude.png",
+    //~ {
+      //~ "frameWidth": 178,
+      //~ "frameHeight": 198
+    //~ }
+  //~ );
   this.load.spritesheet(
-    'dude',
-    'dude.png',
+    "dude",
+    "dude2.png",
     {
-      frameWidth: 178,
-      frameHeight: 198
+      "frameWidth": 178,
+      "frameHeight": 198
     }
   );
 }
 
-function create ()
-{
+function create () {
   // Fundo
-  this.add.image(400, 300, 'sky').setScale(0.4);
+  this.add.image(400, 300, "sky").setScale(0.4);
 
   platforms = this.physics.add.staticGroup();
   // Chão
-  platforms.create(400, 568, 'ground2').setScale(1).refreshBody();
+  platforms.create(400, 568, "ground2").setScale(1).refreshBody();
+  //~ platforms.create(400, 568, "ground2").setScale(2).refreshBody();
   // Plataformas
-  platforms.create(600, 400, 'ground').setScale(1);
-  platforms.create(50, 250, 'ground').setScale(1);
-  platforms.create(750, 220, 'ground').setScale(1);
+  platforms.create(600, 400, "ground").setScale(1);
+  platforms.create(50, 250, "ground").setScale(1);
+  platforms.create(750, 220, "ground").setScale(1);
 
   // Uni
-  player = this.physics.add.sprite(100, 450, 'dude').setScale(0.5);
+  player = this.physics.add.sprite(100, 450, "dude").setScale(0.5);
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
   this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
+    "key": "left",
+    "frames": this.anims.generateFrameNumbers(
+      "dude",
+      {
+        "start": 0,
+        "end": 3
+      }
+    ),
+    "frameRate": 10,
+    "repeat": -1
   });
   this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'dude', frame: 4 } ],
-    frameRate: 20
+    "key": "stand_left",
+    "frames": [
+      {
+        "key": "dude",
+        "frame": 0
+      }
+    ],
+    "frameRate": 20
   });
   this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
+    "key": "jump_left",
+    "frames": [
+      {
+        "key": "dude",
+        "frame": 4
+      }
+    ],
+    "frameRate": 20,
+    "repeat": -1
+  });
+  this.anims.create({
+    "key": "jump_right",
+    "frames": [
+      {
+        "key": "dude",
+        "frame": 5
+      }
+    ],
+    "frameRate": 20,
+    "repeat": -1
+  });
+  this.anims.create({
+    "key": "right",
+    "frames": this.anims.generateFrameNumbers(
+      "dude",
+      {
+        "start": 6,
+        "end": 9
+      }
+    ),
+    "frameRate": 10,
+    "repeat": -1
+  });
+  this.anims.create({
+    "key": "stand_right",
+    "frames": [
+      {
+        "key": "dude",
+        "frame": 6
+      }
+    ],
+    "frameRate": 20
   });
 
   cursors = this.input.keyboard.createCursorKeys();
 
   // Vegcoins
   stars = this.physics.add.group({
-    key: 'star',
-    repeat: 11,
-    setXY: {
-      x: 12,
-      y: 0,
-      stepX: 70
+    "key": "star",
+    "repeat": 11,
+    "setXY": {
+      "x": 12,
+      "y": 0,
+      "stepX": 70
     },
   });
   stars.children.iterate(function (child) {
-    //  Give each star a slightly different bounce
     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
     child.setScale(0.2);
   });
 
   bombs = this.physics.add.group();
 
-  //  The score
+  // Contagem de Vegcoins
   vegcoinsText = this.add.text(
     8,
     8,
-    'VegCoins: 0',
+    "VegCoins: 0",
     {
-      fontSize: '20px',
-      fill: '#fafafa'
+      "fontSize": "20px",
+      "fill": "#fafafa"
     }
   );
   animaisText = this.add.text(
     512,
     8,
-    'Animais prejudicados: 0',
+    "Animais prejudicados: 0",
     {
-      fontSize: '20px',
-      fill: '#fafafa'
+      "fontSize": "20px",
+      "fill": "#fafafa"
     }
   );
   introText = this.add.text(
-    32,
-    128,
+    90,
+    160,
     "Use as setas do teclado para mover\npule com seta para cima!",
     {
-      font: "24px Arial",
-      fill: "#fafafa",
-      align: "center"
+      "font": "24px Arial",
+      "fill": "#fafafa",
+      "align": "center"
     }
   );
 
-  //  Collide the player and the stars with the platforms
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(stars, platforms);
   this.physics.add.collider(bombs, platforms);
 
-  /*  Checks to see if the player overlaps with any of the stars, 
-   * if he does call the collectStar function
-   */
   this.physics.add.overlap(player, stars, collectStar, null, this);
   this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
@@ -193,31 +243,44 @@ function update () {
   if (gameOver) {
     return;
   }
-
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-    player.anims.play('left', true);
-  } else {
-    if (cursors.right.isDown) {
+  if (player.body.touching.down) {
+    if (cursors.up.isDown) {
+      player.setVelocityY(-330);
+      player.anims.play("jump_" + lastMove, true);
+    } else if (cursors.left.isDown) {
+      player.setVelocityX(-160);
+      player.anims.play("left", true);
+      lastMove = "left";
+    } else if (cursors.right.isDown) {
       player.setVelocityX(160);
-      player.anims.play('right', true);
+      player.anims.play("right", true);
+      lastMove = "right";
     } else {
       player.setVelocityX(0);
-      player.anims.play('turn');
+      player.anims.play("stand_" + lastMove);
     }
-  }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
+  } else {
+    if (cursors.left.isDown) {
+      player.setVelocityX(-160);
+      lastMove = "left";
+      player.anims.play("jump_" + lastMove, true);
+    } else if (cursors.right.isDown) {
+      player.setVelocityX(160);
+      lastMove = "right";
+      player.anims.play("jump_" + lastMove, true);
+    } else {
+      player.anims.play("jump_" + lastMove, true);
+    }
   }
 }
 
 function collectStar (player, star) {
-  star.disableBody(true, true);
+  introText.destroy();
 
+  star.disableBody(true, true);
   //  Add and update the score
   vegcoins += 1;
-  vegcoinsText.setText('VegCoins: ' + vegcoins);
+  vegcoinsText.setText("VegCoins: " + vegcoins);
 
   if (stars.countActive(true) === 0) {
     //  A new batch of stars to collect
@@ -225,7 +288,7 @@ function collectStar (player, star) {
       child.enableBody(true, child.x, 0, true, true);
     });
     let x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-    let bomb = bombs.create(x, 16, 'bomb').setScale(0.36);
+    let bomb = bombs.create(x, 16, "bomb").setScale(0.36);
     bomb.setBounce(1);
     bomb.setCollideWorldBounds(true);
     bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
@@ -234,27 +297,13 @@ function collectStar (player, star) {
 }
 
 function hitBomb (player, bomb) {
-  introText.destroy();
   over1Text = this.add.text(200, 300, "Unicórnios", { font: "64px Monospace", fill: "#e8e8e8", align: "center" });
   over2Text = this.add.text(100, 430, "não comem carne!", { font: "64px Monospace", fill: "#e8e8e8", align: "center" });
-  //~ this.add.image(400, 300, 'camiseta').setScale(0.6);
+  //~ this.add.image(400, 300, "camiseta").setScale(0.6);
   this.physics.pause();
   player.setTint(0xff0000);
-  player.anims.play('turn');
+  player.anims.play("jump_" + lastMove);
   gameOver = true;
-}
-
-function checkRatio() {
-    let reRatio = window.innerWidth / window.innerHeight;
-    if (reRatio > ratio) {
-        scale = window.innerHeight / height;
-        reWidth = (window.innerHeight / height) * width;
-        reHeight = window.innerHeigth;
-    } else {
-        scale = window.innerWidth / width;
-        reWidth = window.innerWidth;
-        reHeight = (window.innerWidth / width) * height;
-    }
 }
 
 /**
